551ee0ee4704f9562c2406457f461f2c37098d0c
skocur <skocur10@gmail.com>
2018-06-06 22:19:10 +0200

    Adding basic face recognition

--- ObjectCounter/settings.py
+++ ObjectCounter/settings.py
@@ -114,12 +114,21 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Change it to True in release version
+CSRF_COOKIE_SECURE = False
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates/'),
 )
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- ObjectCounter/views.py
+++ ObjectCounter/views.py
@@ -1,5 +1,24 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.core.files.storage import FileSystemStorage
+
+from PIL import Image
+
+import os
+import cv2
+import numpy as np
+
+def process_file(file):
+    settings_dir = os.path.dirname(__file__)
+    PROJECT_ROOT = os.path.abspath(os.path.dirname(settings_dir))
+
+    # Add additional classifiers based on user input
+    cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+    gray = cv2.cvtColor(np.array(Image.open(PROJECT_ROOT + file)), cv2.COLOR_BGR2GRAY)
+
+    objects = cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=3)
+    
+    return len(objects)
 
 def index(request):
     context = {
@@ -8,4 +27,12 @@ def index(request):
     
     template = loader.get_template('index/index.html')
     
+    if request.method == "POST" and request.FILES['input_image']:
+        file = request.FILES['input_image']
+        fs = FileSystemStorage()
+        filename = fs.save(file.name, file)
+        
+        context['data'] = process_file(fs.url(filename))
+    
+    print(context['data'])
     return HttpResponse(template.render(context, request))
