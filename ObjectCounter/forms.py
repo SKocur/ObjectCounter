8b4ed4be093627db881dad111afbca1c1de0a28c
skocur <skocur10@gmail.com>
2018-06-07 12:36:09 +0200

    Add displaying result image

--- ObjectCounter/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class ImageFileForm(forms.Form):
-    title = forms.CharField(max_length=50)
-    file = forms.FileField()
--- ObjectCounter/views.py
+++ ObjectCounter/views.py
@@ -16,15 +16,24 @@ def process_file(file):
     cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
     gray = cv2.cvtColor(np.array(Image.open(PROJECT_ROOT + file)), cv2.COLOR_BGR2GRAY)
 
-    objects = cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=3)
+    objects = cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=1)
+    
+    for (x, y, w, h) in objects:
+        cv2.rectangle(gray, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    
+    try:
+        cv2.imwrite(PROJECT_ROOT + file, gray)
+    except Exception:
+        print(Exception)
     
     return len(objects)
 
 def index(request):
     context = {
-        "data":"nothing"
-        }
-    
+        "quantity":"0",
+        "result_image": "",
+    }
+        
     template = loader.get_template('index/index.html')
     
     if request.method == "POST" and request.FILES['input_image']:
@@ -32,7 +41,9 @@ def index(request):
         fs = FileSystemStorage()
         filename = fs.save(file.name, file)
         
-        context['data'] = process_file(fs.url(filename))
+        url_to_file = fs.url(filename)
+        
+        context['quantity'] = process_file(url_to_file)
+        context['result_image'] = url_to_file
     
-    print(context['data'])
     return HttpResponse(template.render(context, request))
